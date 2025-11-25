abbd77b38017ec70fc3577a69c7882ddd9ba5f4e
Gaël Maignan <gael.maignan@eivp-paris.fr>
2025-11-25 23:20:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,4 +13,25 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   titles.forEach(h2 => observer.observe(h2));
+
+
+
+
+
+    // Sélectionner tous les éléments avec la classe "appear"
+  const elements = document.querySelectorAll(".appear");
+
+  const observer2 = new IntersectionObserver((entries, obs) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("appear-visible"); // Ajout de la classe pour l'animation
+        obs.unobserve(entry.target); // Empêche l'animation de rejouer
+      }
+    });
+  }, {
+    threshold: 0.2 // Déclenchement lorsque 20% de l'élément est visible
+  });
+
+  elements.forEach(element => observer2.observe(element)); // Observer chaque élément "appear"
 });
+
